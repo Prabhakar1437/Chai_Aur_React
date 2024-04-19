f7ebb23a9e3008a96e86ccd47453f128a44ace08
Prabhakar1437 <prabhakarvatsking@gmail.com>
2024-04-19 10:43:18 +0530

    React Implemantation

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import Chai from "./chai"
 
 function App() {
+  const username = " Vats"
+
   
 
   return (
     <>
     <Chai/>
-    <h1>Chai aur React</h1>
+    <h1>Chai aur React {username}!</h1>
     <p>Test para</p>
     </>
    
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,41 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+
+function MyApp(){
+  return(
+    <div> 
+      <h1>Custom App </h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type:'a',
+//   props:{
+//       href:'https://www.google.com',
+//       target:'_blank'
+//   },
+//   children:'Click me to go to Google!'
+// }
+
+const anotherElement = (
+  <a href = "https://www.google.com" target='_blank'> Visit google</a>
+)
+
+const anotherUser = "Chai aur react"
+
+const reactElement = React.createElement('a', {
+  href:'https://www.google.com',
+  target:'_blank'},
+ 'Go to Google!',
+  anotherUser
+
+
+   )
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  reactElement
+  
 )
